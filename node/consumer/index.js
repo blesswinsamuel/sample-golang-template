f255500655c16f4d915c0317666c270225fa89c7
Blesswin Samuel <bsamuel@digitalocean.com>
2024-02-27 23:16:25 +0530

    Revert "disable node consumer for now"
    This reverts commit 941a7f2fe2d9ace8d1bf886fba8b311ea55f2144.

--- node/consumer/index.js
+++ node/consumer/index.js
@@ -1,27 +1,22 @@
 const fs = require('fs')
 const Kafka = require('node-rdkafka');
-const { setTimeout } = require('node:timers/promises');
 
-setTimeout(7 * 24 * 60 * 60 * 1000).then(() => {
-  console.log('done!')
+const SASL_MECHANISM = 'SCRAM-SHA-256'
+
+fs.writeFileSync('ca-certificate.crt', process.env.KAFKA_CA_CERT, 'utf-8')
+
+const CONSUMER_GROUP = 'test-consumer-group'
+
+const stream = new Kafka.createReadStream({
+  'metadata.broker.list': process.env.KAFKA_BROKER,
+  'group.id': CONSUMER_GROUP,
+  'security.protocol': 'sasl_ssl',
+  'sasl.mechanism': SASL_MECHANISM,
+  'sasl.username': process.env.KAFKA_USERNAME,
+  'sasl.password': process.env.KAFKA_PASSWORD,
+  'ssl.ca.location': 'ca-certificate.crt',
+}, {}, {'topics': [process.env.KAFKA_TOPIC]});
+
+stream.on('data', (message) => {
+  console.log('Got message', message.value.toString());
 });
-
-// const SASL_MECHANISM = 'SCRAM-SHA-256'
-
-// fs.writeFileSync('ca-certificate.crt', process.env.KAFKA_CA_CERT, 'utf-8')
-
-// const CONSUMER_GROUP = 'test-consumer-group'
-
-// const stream = new Kafka.createReadStream({
-//   'metadata.broker.list': process.env.KAFKA_BROKER,
-//   'group.id': CONSUMER_GROUP,
-//   'security.protocol': 'sasl_ssl',
-//   'sasl.mechanism': SASL_MECHANISM,
-//   'sasl.username': process.env.KAFKA_USERNAME,
-//   'sasl.password': process.env.KAFKA_PASSWORD,
-//   'ssl.ca.location': 'ca-certificate.crt',
-// }, {}, {'topics': [process.env.KAFKA_TOPIC]});
-
-// stream.on('data', (message) => {
-//   console.log('Got message', message.value.toString());
-// });
